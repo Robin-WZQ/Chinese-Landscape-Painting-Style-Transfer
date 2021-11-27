fc3ebe51870c6ca1a7c6ab2dba7ad12e86c3561c
Robin-WZQ <60317828+Robin-WZQ@users.noreply.github.com>
2021-11-27 18:31:48 +0800

    Add files via upload

---
+++ tools/canny.py
@@ -0,0 +1,209 @@
+import os
+
+import cv2 as cv
+import numpy as np
+from edge_enhancement import enhancement
+
+import sys
+sys.setrecursionlimit(100000)  
+
+def smooth(image, sigma = 1.4, length = 5):
+    """ 
+    利用高斯核进行平滑处理
+
+    Args:
+        image: array of grey image
+        sigma: the sigma of gaussian filter, default to be 1.4
+        length: the kernal length, default to be 5
+
+    Returns:
+        the smoothed image
+    """
+    # 计算高斯核
+    k = length // 2
+    gaussian = np.zeros([length, length])
+    for i in range(length):
+        for j in range(length):
+            gaussian[i, j] = np.exp(-((i-k) ** 2 + (j-k) ** 2) / (2 * sigma ** 2))
+    gaussian /= 2 * np.pi * sigma ** 2
+    # Batch 归一化
+    gaussian = gaussian / np.sum(gaussian)
+
+    # 使用高斯核
+    W, H = image.shape
+    new_image = np.zeros([W - k * 2, H - k * 2])
+
+    for i in range(W - 2 * k):
+        for j in range(H - 2 * k):
+            # 卷积运算
+            new_image[i, j] = np.sum(image[i:i+length, j:j+length] * gaussian)
+
+    new_image = np.uint8(new_image)
+    return new_image
+
+def get_gradient_and_direction(image):
+    """ 
+    利用sobel算子计算梯度和方向
+
+    Args:
+        image: array of grey image
+
+    Returns:
+        gradients: the gradients of each pixel
+        direction: the direction of the gradients of each pixel
+    """
+    Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    Gy = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+    W, H = image.shape
+    gradients = np.zeros([W - 2, H - 2])
+    direction = np.zeros([W - 2, H - 2])
+
+    for i in range(W - 2):
+        for j in range(H - 2):
+            dx = np.sum(image[i:i+3, j:j+3] * Gx)
+            dy = np.sum(image[i:i+3, j:j+3] * Gy)
+            gradients[i, j] = np.sqrt(dx ** 2 + dy ** 2)
+            if dx == 0:
+                direction[i, j] = np.pi / 2
+            else:
+                direction[i, j] = np.arctan(dy / dx)
+
+    gradients = np.uint8(gradients)
+    return gradients, direction
+
+def NMS(gradients, direction):
+    """ 
+    非极大值抑制
+
+    Args:
+        gradients: the gradients of each pixel
+        direction: the direction of the gradients of each pixel
+
+    Returns:
+        the output image
+    """
+    W, H = gradients.shape
+    nms = np.copy(gradients[1:-1, 1:-1])
+
+    for i in range(1, W - 1):
+        for j in range(1, H - 1):
+            theta = direction[i, j]
+            weight = np.tan(theta)
+            if theta > np.pi / 4:
+                d1 = [0, 1]
+                d2 = [1, 1]
+                weight = 1 / weight
+            elif theta >= 0:
+                d1 = [1, 0]
+                d2 = [1, 1]
+            elif theta >= - np.pi / 4:
+                d1 = [1, 0]
+                d2 = [1, -1]
+                weight *= -1
+            else:
+                d1 = [0, -1]
+                d2 = [1, -1]
+                weight = -1 / weight
+
+            g1 = gradients[i + d1[0], j + d1[1]]
+            g2 = gradients[i + d2[0], j + d2[1]]
+            g3 = gradients[i - d1[0], j - d1[1]]
+            g4 = gradients[i - d2[0], j - d2[1]]
+
+            grade_count1 = g1 * weight + g2 * (1 - weight)
+            grade_count2 = g3 * weight + g4 * (1 - weight)
+
+            if grade_count1 > gradients[i, j] or grade_count2 > gradients[i, j]:
+                nms[i - 1, j - 1] = 0
+    return nms
+
+def double_threshold(nms, threshold1, threshold2):
+    """ 
+    两个边界计算
+
+    Args:
+        nms: the input image
+        threshold1: the low threshold
+        threshold2: the high threshold
+
+    Returns:
+        The binary image.
+    """
+    visited = np.zeros_like(nms)
+    output_image = nms.copy()
+    W, H = output_image.shape
+
+    def dfs(i, j):
+        if i >= W or i < 0 or j >= H or j < 0 or visited[i, j] == 1:
+            return
+        visited[i, j] = 1
+        if output_image[i, j] > threshold1:
+            output_image[i, j] = 255
+            dfs(i-1, j-1)
+            dfs(i-1, j)
+            dfs(i-1, j+1)
+            dfs(i, j-1)
+            dfs(i, j+1)
+            dfs(i+1, j-1)
+            dfs(i+1, j)
+            dfs(i+1, j+1)
+        else:
+            output_image[i, j] = 0
+
+    for w in range(W):
+        for h in range(H):
+            if visited[w, h] == 1:
+                continue
+            if output_image[w, h] >= threshold2:
+                dfs(w, h)
+            elif output_image[w, h] <= threshold1:
+                output_image[w, h] = 0
+                visited[w, h] = 1
+
+    for w in range(W):
+        for h in range(H):
+            if visited[w, h] == 0:
+                output_image[w, h] = 0
+    return output_image
+
+def processing(img_path):
+
+    image = cv.imread(img_path,0)
+    image = enhancement(image)
+    smoothed_image = smooth(image)
+    gradients, direction = get_gradient_and_direction(smoothed_image)
+    nms = NMS(gradients, direction)
+    output_image = double_threshold(nms, 40, 100)
+
+    return output_image
+
+def main(input_path, output_path):
+    '''
+    主函数
+    '''
+    files_path = []
+    for label in sorted(os.listdir(input_path)): #label：来源哪个数据集
+        for fname in os.listdir(os.path.join(input_path, label)):
+            files_path.append(os.path.join(input_path, label, fname)) #图片的文件名
+
+    for file_path in files_path:
+        all = file_path.split("\\")
+        label = all[-2] #获取数据集名，如met-1
+        fname = all[-1] #获取文件名，如met_0.jpg
+        arguments_strOut = os.path.join(output_path, label, fname)
+
+        image = processing(file_path)
+
+        cv.imwrite(arguments_strOut,image)
+
+
+if __name__ == "__main__":
+    
+    input_path = "E:/junior/CV/Homework/term_project/generate/shashui/Landscape_GAN/pictures" #输入路径
+    os.makedirs("canny_pic/" , exist_ok=True)
+    names = ['Harvard','met-1','met-2','Princeton-1','Princeton-2','Smithsonian-1','Smithsonian-2','Smithsonian-3','Smithsonian-4','Smithsonian-5']
+    for name in names:
+        os.makedirs("canny_pic/%s" % name , exist_ok=True)
+    output_path = "canny_pic" #输出路径
+    main(input_path, output_path)
---
+++ tools/picture2texture.py
@@ -0,0 +1,184 @@
+#this file is modified by Zhongqi Wang from https://github.com/sniklaus/pytorch-hed
+
+#!/usr/bin/env python
+
+import getopt
+import os
+import sys
+
+import cv2
+import numpy
+import PIL
+import PIL.Image
+import torch
+
+##########################################################
+
+assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 13) # requires at least pytorch version 1.3.0
+
+torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
+
+torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+##########################################################
+
+arguments_strModel = 'bsds500' # only 'bsds500' for now
+
+
+for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
+	if strOption == '--model' and strArgument != '': arguments_strModel = strArgument # which model to use
+	if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
+	if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
+# end
+
+##########################################################
+
+class Network(torch.nn.Module):
+	def __init__(self):
+		super().__init__()
+
+		self.netVggOne = torch.nn.Sequential(
+			torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggTwo = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggThr = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggFou = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggFiv = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+
+		self.netCombine = torch.nn.Sequential(
+			torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
+			torch.nn.Sigmoid()
+		)
+
+		self.load_state_dict({ strKey.replace('module', 'net'): tenWeight for strKey, tenWeight in torch.hub.load_state_dict_from_url(url='http://content.sniklaus.com/github/pytorch-hed/network-' + arguments_strModel + '.pytorch', file_name='hed-' + arguments_strModel).items() })
+	# end
+
+	def forward(self, tenInput):
+		tenBlue = (tenInput[:, 0:1, :, :] * 255.0) - 104.00698793
+		tenGreen = (tenInput[:, 1:2, :, :] * 255.0) - 116.66876762
+		tenRed = (tenInput[:, 2:3, :, :] * 255.0) - 122.67891434
+
+		tenInput = torch.cat([ tenBlue, tenGreen, tenRed ], 1)
+
+		tenVggOne = self.netVggOne(tenInput)
+		tenVggTwo = self.netVggTwo(tenVggOne)
+		tenVggThr = self.netVggThr(tenVggTwo)
+		tenVggFou = self.netVggFou(tenVggThr)
+		tenVggFiv = self.netVggFiv(tenVggFou)
+
+		tenScoreOne = self.netScoreOne(tenVggOne)
+		tenScoreTwo = self.netScoreTwo(tenVggTwo)
+		tenScoreThr = self.netScoreThr(tenVggThr)
+		tenScoreFou = self.netScoreFou(tenVggFou)
+		tenScoreFiv = self.netScoreFiv(tenVggFiv)
+
+		tenScoreOne = torch.nn.functional.interpolate(input=tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreTwo = torch.nn.functional.interpolate(input=tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreThr = torch.nn.functional.interpolate(input=tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+
+		return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
+	# end
+# end
+
+netNetwork = None
+
+##########################################################
+
+def estimate(tenInput):
+	global netNetwork
+
+	if netNetwork is None:
+		netNetwork = Network().to(device).eval()
+	# end
+
+	intWidth = tenInput.shape[2]
+	intHeight = tenInput.shape[1]
+
+	assert(intWidth == 512) # 调整为本次图像输入大小
+	assert(intHeight == 512) # 调整为本次图像输入大小
+
+	return netNetwork(tenInput.to(device).view(1, 3, intHeight, intWidth))[0, :, :, :].cpu()
+# end
+
+##########################################################
+
+if __name__ == '__main__':
+	files_path = []
+	input_path = "E:/junior/CV/Homework/term_project/generate/shashui/Landscape_GAN/pic2" #输入路径
+	os.makedirs("processing_pic/" , exist_ok=True)
+	names = ['Harvard','met-1','met-2','Princeton-1','Princeton-2','Smithsonian-1','Smithsonian-2','Smithsonian-3','Smithsonian-4','Smithsonian-5']
+	for name in names:
+		os.makedirs("processing_pic/%s" % name , exist_ok=True)
+	output_path = "E:/junior/CV/Homework/term_project/generate/shashui/Landscape_GAN/processing_pic" #输出路径
+
+
+	for label in sorted(os.listdir(input_path)): #label：来源哪个数据集
+		for fname in os.listdir(os.path.join(input_path, label)):
+			files_path.append(os.path.join(input_path, label, fname)) #图片的文件名
+	
+	for file_path in files_path:
+		arguments_strIn = file_path
+		all = file_path.split("\\")
+		label = all[-2] #获取数据集名，如met-1
+		fname = all[-1] #获取文件名，如met_0.jpg
+		arguments_strOut = os.path.join(output_path, label, fname)
+		try:
+			tenInput = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(arguments_strIn))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+			tenOutput = estimate(tenInput)
+			PIL.Image.fromarray((tenOutput.clip(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(arguments_strOut)
+		except:
+			'''若是灰度图需要特别处理！'''
+			src = cv2.imread(file_path, 0)
+			src_RGB = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB)
+			a = PIL.Image.fromarray(src_RGB)
+			b = numpy.array(a)[:, :]
+			tenInput = torch.FloatTensor(numpy.ascontiguousarray(b.transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+			tenOutput = estimate(tenInput)
+			PIL.Image.fromarray((tenOutput.clip(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(arguments_strOut)
+		
