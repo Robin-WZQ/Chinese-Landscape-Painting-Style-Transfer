a621806154e36224fbe2aa6b29d9e63b13c75393
Robin-WZQ <60317828+Robin-WZQ@users.noreply.github.com>
2021-11-27 18:12:20 +0800

    Update opts.py

--- opts.py
+++ opts.py
@@ -12,13 +12,13 @@ def parse_opts():
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--img_height", type=int, default=128, help="size of image height")
-    parser.add_argument("--img_width", type=int, default=128, help="size of image width")
+    parser.add_argument("--img_height", type=int, default=512, help="size of image height")
+    parser.add_argument("--img_width", type=int, default=512, help="size of image width")
     parser.add_argument("--channels", type=int, default=3, help="number of image channels")
     parser.add_argument(
         "--sample_interval", type=int, default=3000, help="interval between sampling of images from generators"
     )
-    parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
+    parser.add_argument("--img_size", type=int, default=512, help="size of each image dimension")
     parser.add_argument("--checkpoint_interval", type=int, default=5, help="interval between model checkpoints")
     parser.add_argument("--output_path",type=str,default="pictures",help='Local path of images')
     parser.add_argument("--texture_path",type=str,default="train_data/",help='Local path of textures')
